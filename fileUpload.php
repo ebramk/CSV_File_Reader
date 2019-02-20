79af289233f7391af3dc357a30a15dbb9ca7c089
Ebramk <godislove12>
2019-02-19 20:24:13 -0500

    added file name, extension, temp file name variables to store the data.

--- fileUpload.php
+++ fileUpload.php
@@ -5,3 +5,19 @@
  * Date: 2/19/2019
  * Time: 8:17 PM
  */
+
+
+<?php
+    $currentDir = getcwd();
+    $uploadDirectory = "/";
+
+    $errors = []; // Store all foreseen and unforseen errors here
+
+    $fileExtensions = ['jpeg','jpg','png','xls','csv']; // Get all the file extensions
+
+    $fileName = $_FILES['myfile']['name'];
+    $fileSize = $_FILES['myfile']['size'];
+    $fileTmpName  = $_FILES['myfile']['tmp_name'];
+    $fileType = $_FILES['myfile']['type'];
+    $fileExtension = strtolower(end(explode('.',$fileName)));
+	$file_name = 'csv.csv';
