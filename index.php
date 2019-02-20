98ee91f2329fc9bc4b0f96b87d462688748b1b81
Ebramk <godislove12>
2019-02-19 21:40:37 -0500

    align header text 1

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>File Upload with PHP</title>
+    <title>CSV File Reader</title>
 </head>
 <body>
 <div class="container">
     <div class="jumbotron">
-        <h1 align="center">File Upload with PHP</h1>
+        <h1 align="center">CSV File Reader</h1>
         <p align="center"> This is a simple PHP program that reads a user uploaded scv file and displays it as an HTML table.</p>
     </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
