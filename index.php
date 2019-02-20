edf737b6c92ea956a99d5a9b0b5a5d79644f8e91
Ebramk <godislove12>
2019-02-19 21:38:32 -0500

    align header text 1

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <div class="container">
     <div class="jumbotron">
         <h1 align="center">File Upload with PHP</h1>
-        <p>This is a simple PHP program that reads a user uploaded scv file and displays it as an HTML table.</p>
+        <p align="center"> This is a simple PHP program that reads a user uploaded scv file and displays it as an HTML table.</p>
     </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
