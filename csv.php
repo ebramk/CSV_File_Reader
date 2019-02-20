9cc38874272493cdfa0280f727fec46a6ee9bc36
Ebramk <godislove12>
2019-02-19 20:13:18 -0500

    Adding an upload file button. This will allow users to select a file from their PC and post it on heroku. Simple HTML form.

--- csv.php
+++ csv.php
@@ -1,41 +1,14 @@
-<?php
-
-$row = 1;
-if (($handle = fopen("test.csv", "r")) !== FALSE) {
-
-    echo '<table border="1">';
-
-    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-        $num = count($data);
-        if ($row == 1) {
-            echo '<thead><tr>';
-        }else{
-            echo '<tr>';
-        }
-        
-        for ($c=0; $c < $num; $c++) {
-            //echo $data[$c] . "<br />\n";
-            if(empty($data[$c])) {
-               $value = "&nbsp;";
-            }else{
-               $value = $data[$c];
-            }
-            if ($row == 1) {
-                echo '<th>'.$value.'</th>';
-            }else{
-                echo '<td>'.$value.'</td>';
-            }
-        }
-
-        if ($row == 1) {
-            echo '</tr></thead><tbody>';
-        }else{
-            echo '</tr>';
-        }
-        $row++;
-    }
-
-    echo '</tbody></table>';
-    fclose($handle);
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>File Upload with PHP</title>
+</head>
+<body>
+<form action="fileUpload.php" method="post" enctype="multipart/form-data">
+    Upload a File:
+    <input type="file" name="myfile" id="fileToUpload">
+    <input type="submit" name="submit" value="Upload File Now" >
+</form>
+</body>
+</html>
