111708344f484d25b4f7d1546c6baeb139ff7d5a
Ebramk <godislove12>
2019-02-19 21:37:25 -0500

    align header text

--- index.php
+++ index.php
@@ -18,6 +18,10 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+
+    <div class="alert alert-primary" role="alert">
+        Please upload a CSV file to get started
+    </div>
 <form action="fileUpload.php" method="post" enctype="multipart/form-data">
     Upload a File:
     <input type="file" name="myfile" id="fileToUpload">
